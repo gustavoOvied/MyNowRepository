fd4be137c692ce4633d4041635fc8b86625aa552
Gustavo Oviedo <andruscespedes2@gmail.com>
2021-09-07 20:12:01 +0000

    Archivo base

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace SintaxisBasicaC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\t Sintaxis Basica de C#\n");
+
+            Console.WriteLine("Para mostrar por pantalla el comando es:  Console.WriteLine(texto en comillas dobles)\n");
+
+            Console.WriteLine("Si escribimos (cw ; presionamos Tab + Tab) nos escribe automatico el comando Console.WriteLine()\n");
+
+            Console.WriteLine("El (namespace) = Es el indicar de como se llama nuestro proyecto\n");
+
+            Console.WriteLine("Las { } = Nos indica la apertura y cierre de una clase o Metodo/funcion \n");
+
+            Console.WriteLine("Operadores Matematicos ( + , - , * , / )\n");
+
+            Console.WriteLine("Operadores de comparacion ( < , > , = )\n");
+
+            Console.WriteLine("Para unir cadenas en una misma linea se usa +\n");
+
+            Console.WriteLine("Se usa el IF = Para poder hacer una condicion, si se cumple haga tal cosa.\n");
+
+            Console.WriteLine("Declarar una variable con ( int , double , strings, float , debemos decir el tipo )\n");
+
+            Console.WriteLine("C# distinge Mayusculas y Minusculas. \n");
+
+            Console.WriteLine("Para ejecutar una aplicacion = Depurar>Iniciar sin depurar. o Ctrl + F5 \n");
+
+            Console.WriteLine("Para poner un comentario se utiliza // antes de la linea de comandos\n");
+
+            Console.WriteLine("Poner el mismo valor a varias variables se usa el = entre estas, y luego el valor deseado\n");
+
+            Console.WriteLine("El var = detecta el tipo de las variables\n");
+
+            Console.WriteLine("Se usa el int.Parse() para convertir el valor strings a int\n");
+
+            Console.WriteLine("Usamos const para que en todo el codigo siga igual, en una misma linea se puede declarar\n");
+
+            Console.WriteLine("La funcion Math. nos facilita varias formulas matematicas, ejemplo el area Math.Pow(base, exponente)\n");
+
+            Console.WriteLine("En una clase podemos reemplazar el return por =>, que seria lo mismo\n");
+
+            Console.WriteLine("Un metodo nos ayuda hacer operaciones apartes del Main\n");
+
+            Console.WriteLine("Podemos sobrecargar metodos, o sea poner el mismo nombre pero con parametros diferentes\n");
+
+            Console.WriteLine("La funcion booleana = ( bool) nos indica si es false o true\n");
+
+            Console.WriteLine("El operador de negacion invierte los valores este es = !\n");
+
+            Console.WriteLine("Podemos utilizar estas expresiones para hacer bucles: while, do while, for \n");
+
+            Console.WriteLine("Para las excepcion utilizamos el try y catch; en el catch podemos cual es la excepcion\n");
+
+            Console.WriteLine("Algunas operaciones que generan excepcion son saltadas, para usamos o activamos el checked.\n");
+
+            Console.WriteLine("Si queremos que un codigo se ejecute siempre podemos usar el codigo Finally\n");
+
+            Console.WriteLine("Podemos crear clases, objetos, Metodos, esto la podemos Encapsular.\n");
+
+            Console.WriteLine("Como crear una clase = class Circulo\n");
+
+            Console.WriteLine("Como hacer un objeto = Circulo miCirculo = new Circulo( ) este es el que dice el comportamiento del metodo\n");
+
+            Console.WriteLine("Com hacer un Metodo = public double calculoArea(int radio){ \n" + "return pi * radio * radio \n}" + "\n este hace una funcion.\n");
+
+            
+        }
+    }
+}
